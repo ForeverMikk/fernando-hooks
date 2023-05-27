466aa624c9ff89b718cf948d4cea58cbaf6be628
unknown <michaelmolina679@gmail.com>
2023-05-27 12:28:16 -0600

    usando 2 custom hooks useCounter y useFetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,31 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
   
-  const {data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
-  
-  console.log(data, isLoading, hasError);
-  
+  const { counter, increment} = useCounter();
+  const { data, isLoading } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+  const { author, quote } = !!data && data[0]; //con el doble signo !! vuelves false el valor del null 
   return (
     <>
         <h1>Braking Bad Cuotes</h1>
         <hr />
+          {
+            isLoading 
+            ? <h2 className="alert alert-info text-center">Cargando...</h2>
+            : <blockquote className="blockquote text-end">
+              <p className="mb-1">{quote}</p>
+              <footer className="blockquote-footer mt-2">{author}</footer>
+            </blockquote>
+          }
+
+          <button 
+            onClick={() => increment(1)} 
+            className="btn btn-primary"
+            disabled={isLoading}
+          >
+            Next Quote
+          </button>
     </>
   )
 }
