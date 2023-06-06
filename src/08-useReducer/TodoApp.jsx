d411442f707b15e887e6653774d5a2b9f3e8d356
unknown <michaelmolina679@gmail.com>
2023-06-05 18:46:17 -0600

    Estructura de TODOApp pra el proyecto

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -18,18 +18,39 @@ const initialState = [
 
 export const TodoApp = () => {
 
-
     const [state, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <>
-        <h1>TODO App</h1>
+        <h1>TODOApp: 10 <small>pendientes 2</small> </h1>
         <hr />
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+        <div className="row">
+            <div className="col-7">
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        <span>Item 1</span>
+                        <button className="btn btn-danger">Borrar</button>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="col-5">
+                <h4>Agregar TODO</h4>
+                <hr />
+                <form action="">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Que hay que hacer"
+                    />
+                    <button 
+                        type="submit"
+                        className="btn btn-primary mt-2">
+                        Agregar
+                    </button>
+                </form>
+            </div>
+        </div>
     </>
   )
 }
