69a7a14b65f47a2cc830d4a7296ec1c9f9ed05dd
unknown <michaelmolina679@gmail.com>
2023-06-09 12:22:49 -0600

    guardando la data en el local Storage

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,24 +1,36 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./totoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del poder',
-        done: false,
-    }
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // },
+    // {
+    //     id: new Date().getTime() * 3,
+    //     description: 'Recolectar la piedra del poder',
+    //     done: false,
+    // }
 ]
+
+const init = () => {
+    // Esta funcion indica que si el valor del arreglo es null, regresa un arreglo vacio
+    // Si no, regresa el valor del local Storage
+    return JSON.parse(localStorage.getItem('todos')) || []; 
+
+}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos)); //Seteas los valores del localStorage cada que cambian los todos
+    }, [todos]);
+    
 
     const handleNewTodo = (todo) => {
         const action = {
