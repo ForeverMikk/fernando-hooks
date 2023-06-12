c4234a7b6a1b755f7a3167647abbd03011c77b08
unknown <michaelmolina679@gmail.com>
2023-06-12 16:49:33 -0600

    useContext muy importante

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: '123',
-    name: 'Michael',
-    email: 'correo@gmail.com'
-}
+// const user = {
+//     id: '123',
+//     name: 'Michael',
+//     email: 'correo@gmail.com'
+// }
 
 
 export const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState()
   return (
     // Cualquier hijo de este Provider puede acceder a las propiedades del value
-   <UserContext.Provider value={user}> 
+   <UserContext.Provider value={{user, setUser}}> 
     {children}
    </UserContext.Provider>
   )
