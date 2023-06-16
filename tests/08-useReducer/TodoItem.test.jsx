eef8a4e940fa39cdeb6e4329cffe7ed5bacf3728
unknown <michaelmolina679@gmail.com>
2023-06-16 17:14:13 -0600

    Provando eventos en <TodoItem />

--- tests/08-useReducer/TodoItem.test.jsx
+++ tests/08-useReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
 /* eslint-disable no-undef */
@@ -27,8 +27,55 @@ describe('Pruebas en <TodoItem />', () => {
 
         const spanElement = screen.getByLabelText('span');
         expect(spanElement.className).toBe('');
-        // screen.debug();
-
-
     });
+
+    test('debe de mostrar Todo completado', () => { 
+        
+        todo.done = true;
+        
+        render(
+            <TodoItem 
+                todo={todo} 
+                onToggleTodo={onToggleTodoMock} 
+                onDeleteTodo={onDeleteTodoMock}
+            />)
+
+        const liElement = screen.getByRole('listitem');
+        expect(liElement.className).toBe('list-group-item d-flex justify-content-between align-items-center'); //Checas las clases del li
+
+        const spanElement = screen.getByLabelText('span');
+        expect(spanElement.className).toContain('text-decoration-line-through');
+    });
+
+    test('el span debe de llamar el toggleTodo cuando hace click', () => { 
+
+        render(
+            <TodoItem 
+                todo={todo} 
+                onToggleTodo={onToggleTodoMock} 
+                onDeleteTodo={onDeleteTodoMock}
+            />)
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click(spanElement);
+
+        expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id); 
+    });
+
+    test('el span debe de llamar el deleteTodo cuando hace click', () => { 
+
+        render(
+            <TodoItem 
+                todo={todo} 
+                onToggleTodo={onToggleTodoMock} 
+                onDeleteTodo={onDeleteTodoMock}
+            />)
+
+        const deleteButton = screen.getByRole('button');
+        fireEvent.click(deleteButton);
+
+        expect(onDeleteTodoMock).toHaveBeenCalledWith(todo.id); 
+    });
+
+
 });
