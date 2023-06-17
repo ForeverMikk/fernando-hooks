9b7ddce26b4bf43a2bb5ac02b675409026cd3fa6
unknown <michaelmolina679@gmail.com>
2023-06-17 10:32:55 -0600

    Pruebas en useContext <HomePage />

---
+++ tests/09-useContext/HomePage.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { HomePage } from "../../src/09-useContext/HomePage";
+import { UserContext } from "../../src/09-useContext/context/UserContext";
+
+/* eslint-disable no-undef */
+describe('Pruebas en <HomePage />', () => { 
+
+    const user = {
+        id: 1,
+        name: 'Michael'
+    }
+
+    test('debe mostrar el componente sin el usuario', () => { 
+
+        render(
+            <UserContext.Provider value={{user: null}}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); //aria-label
+        expect(preTag.innerHTML).toBe('null');
+    });
+
+    test('debe mostrar el componente CON el usuario', () => { 
+
+        render(
+            <UserContext.Provider value={{user: user}}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); 
+        expect(preTag.innerHTML).toContain(user.name);
+        expect(preTag.innerHTML).toContain(user.id.toString());
+    });
+});
